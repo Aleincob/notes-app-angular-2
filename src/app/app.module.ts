4c3c62c07ed625f43ba5af500c1dea9d2f2163bb
Alejandro <alecaperez13@gmail.com>
2023-01-21 20:21:21 -0500

    updated styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 import { HeaderComponent } from './components/header/header.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AddNotePageComponent } from './components/add-note-page/add-note-page.component';
@@ -27,6 +29,8 @@ import { AddNotePageComponent } from './components/add-note-page/add-note-page.c
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatCardModule,
+    MatDividerModule,
     ReactiveFormsModule,
   ],
   bootstrap: [AppComponent]
